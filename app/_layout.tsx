e1e12439f5672e799816036248d52853dbb9aff5
Roniai <valitera.fehizoro@gmail.com>
2025-01-16 15:19:39 +0300

    feat(drawer): add all screens with style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,57 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
+import {
+  CustomDrawerContent,
+  ItemDrawer,
+} from "@/components/navigation/drawer-navigation/drawer-content/CustomDrawerContent";
+import { Color } from "@/styles";
+import { screensDrawer } from "@/assets/data/screens-drawer";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins_Regular: require("../assets/fonts/Poppins-Regular.ttf"),
+    Poppins_Medium: require("../assets/fonts/Poppins-Medium.ttf"),
+    Poppins_Bold: require("../assets/fonts/Poppins-Bold.ttf"),
+    Poppins_SemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+    Poppins_ExtraBold: require("../assets/fonts/Poppins-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
-        <Drawer.Screen
-          name="index"
-          options={{
-            drawerLabel: "Home",
-            title: "Accueil",
-          }}
-        />
-        <Drawer.Screen
-          name="about"
-          options={{
-            drawerLabel: "About",
-            title: "A propos",
-          }}
-        />
+      <Drawer
+        screenOptions={{
+          drawerActiveTintColor: Color.greyscaleDarker,
+        }}
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+        {screensDrawer.map((screen, index) => (
+          <Drawer.Screen
+            key={index}
+            name={screen.name}
+            options={{
+              drawerIcon: () => (
+                <MaterialCommunityIcons name={screen.icon as any} size={30} />
+              ),
+              drawerLabel: () => <ItemDrawer label={screen.label} />,
+              title: screen.title,
+            }}
+          />
+        ))}
       </Drawer>
     </GestureHandlerRootView>
   );
---
+++ app/list-lyrics.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const ListLyrics = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>ListLyrics</Text>
+    </View>
+  );
+};
+
+export default ListLyrics;
