987dfe88c128ce2de0dcf61411fc02b76b1d21a7
Roniai <valitera.fehizoro@gmail.com>
2025-02-26 16:02:18 +0300

    refactor(home): use variable for title hard text

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import BibleVerse from "@/components/BibleVerse";
 import Title from "@/components/Title";
-import { BODY_TEXT, VERSES_TEXT } from "@/constants/Texts";
+import { BODY_TEXT, TITLE, VERSES_TEXT } from "@/constants/Texts";
 import { Color, Font, Size } from "@/styles";
 import { StyleSheet, Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <ScrollView style={styles.mainContainer}>
       <View style={styles.bodyContainer}>
-        <Title title={"TENY FANOLORANA"} />
+        <Title title={TITLE.intro} />
         <BibleVerse verseBody={VERSES_TEXT.intro} />
         <Text style={Font.subtitle}>{BODY_TEXT.intro}</Text>
       </View>
