7c6760f6cca32a97e7fbfddc081b36d14995be6e
Roniai <valitera.fehizoro@gmail.com>
2025-02-27 08:38:23 +0300

    feat(about): create about page

--- app/about.tsx
+++ app/about.tsx
@@ -1,11 +1,81 @@
-import { Text, View } from "react-native";
+import { Avatar } from "@/components/Avatar";
+import BibleVerse from "@/components/BibleVerse";
+import Title from "@/components/Title";
+import { AVATAR_DATA } from "@/constants/Images";
+import {
+  BODY_TEXT,
+  CONTACT_ME,
+  LABEL,
+  TITLE,
+  VERSES_TEXT,
+} from "@/constants/Texts";
+import { Color, Font, Size } from "@/styles";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Linking, StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const About = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>About</Text>
-    </View>
+    <>
+      <ScrollView style={styles.mainContainer}>
+        <View style={styles.bodyContainer}>
+          <Title title={TITLE.about} />
+          <BibleVerse verseBody={VERSES_TEXT.about} />
+
+          <Avatar
+            imageSource={AVATAR_DATA.me.uri}
+            name={AVATAR_DATA.me.name}
+            title={AVATAR_DATA.me.title}
+            width={50}
+            height={50}
+            style={{ alignSelf: "center" }}
+          />
+          <View style={styles.contactContainer}>
+            <MaterialCommunityIcons
+              name="phone"
+              size={50}
+              onPress={() => Linking.openURL(`tel:${CONTACT_ME.phone}`)}
+            />
+            <MaterialCommunityIcons
+              name="gmail"
+              size={50}
+              onPress={() => Linking.openURL(`mailto:${CONTACT_ME.email}`)}
+            />
+            <MaterialCommunityIcons
+              name="facebook"
+              size={50}
+              onPress={() => Linking.openURL(`${CONTACT_ME.facebook}`)}
+            />
+          </View>
+          <Text style={Font.subtitle}>{BODY_TEXT.about}</Text>
+        </View>
+      </ScrollView>
+      <Text style={styles.versionText}>{LABEL.version}</Text>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: Color.white,
+  },
+  bodyContainer: {
+    marginHorizontal: Size.L,
+    marginVertical: Size.M,
+    gap: Size.M,
+  },
+  contactContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  versionText: {
+    ...Font.caption,
+    position: "absolute",
+    bottom: Size.XS,
+    right: Size.XS,
+  },
+});
 
 export default About;
--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -6,28 +6,30 @@ import {
   StyleSheet,
   Text,
   View,
-  ViewProps,
+  ViewStyle,
 } from "react-native";
 
 type Props = {
   name?: string;
   title?: string;
   imageSource: string;
-  styleImage?: StyleProp<ImageProps>;
-  style?: StyleProp<ViewProps>;
+  width?: number;
+  height?: number;
+  style?: StyleProp<ViewStyle>;
 };
 
 export const Avatar: React.FC<Props> = ({
   imageSource,
   name,
   title,
-  styleImage,
+  width = 28,
+  height = 28,
   style,
 }) => {
   return (
-    <View style={[style, { width: vw * 28 }]}>
+    <View style={[style, { width: width * vw }]}>
       <ImageBackground
-        style={[styles.pdpImage, styleImage]}
+        style={[styles.pdpImage, { height: height * vw }]}
         resizeMode="cover"
         source={imageSource as ImageProps}
       />
@@ -47,7 +49,6 @@ const styles = StyleSheet.create({
   },
   pdpImage: {
     width: "100%",
-    height: vw * 28,
     backgroundColor: Color.greyscaleSubtle,
     borderColor: Color.white,
     borderWidth: 1 * vw,
